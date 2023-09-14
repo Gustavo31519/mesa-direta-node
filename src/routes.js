972877f041216db491b46cfc01fa41d674c5421e
Gustavo31519 <lsbgustavo@gmail.com>
2023-09-14 20:38:53 -0300

    Objetos JSON, Clientes sendo cadastrados e Aparecem na tela

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,19 @@
 const { Router } = require("express");
 const  db  = require("./services/mysql");
-const allUser = require("./helpers/handleMysql");
+const {allUser, insertUser} = require("./helpers/handleMysql");
 const routes = Router();
-
 
 routes.get("/select", async (req, res) => {
   try {
      res.json((await db.dbPromise.query(allUser))[0]);
   } catch (e) {
     res.status(500).json({ message: "Erro interno do servidor" });
-  }
+  } 
 });
 
 routes.post("/insert", async (req, res) => {
   try {
-    const result = await db.dbPromise.query(
-      "INSERT INTO user (name, email) VALUES (?, ?)",
+    const result = await db.dbPromise.query(insertUser,
       [clientInformations[0], clientInformations[1]]
     );
     res.json({
@@ -27,26 +25,18 @@ routes.post("/insert", async (req, res) => {
   }
 });
 
-
-
 routes.post("/receber", async (req, res) => {
   const clientInformations = req.body.clientInformations;
-  console.log("Informação recebida", clientInformations);
+  console.log("Informação recebida" + clientInformations);
 
   try {
-    const result = await db.dbPromise.query(
-      "INSERT INTO user (name, email) VALUES (?, ?)",
+    const result = await db.dbPromise.query(insertUser,
       [clientInformations[0], clientInformations[1]]
     );
-    console.log({
-      message: "Usuário inserido com sucesso",
-      insertedId: result[0].insertId,
-    });
   } catch (e) {
     console.error("Erro interno do servidor:", e);
   }
 });
-
 
 
 module.exports = routes
